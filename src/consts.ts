f99f736c37e1850ed42ee5e2568aa88c1ad74b75
Trey Granderson <treygranderson@gmail.com>
2024-04-28 02:16:20 -0400

    page titles and descriptions

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
+  NAME: "TG",
   EMAIL: "markhorn.dev@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 3,
@@ -9,24 +9,26 @@ export const SITE: Site = {
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  TITLE: "Trey Granderson | Front End Engineer & Web Developer",
+  DESCRIPTION:
+    "Trey Granderson - Front End Engineer | Experienced in building high-performance, accessible web applications using React, TypeScript, and modern web technologies. Discover how I leverage cutting-edge tools and methodologies to create intuitive and scalable digital solutions that drive user engagement and business success.",
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
+  TITLE: "Trey Granderson’s Blog on Front End Development and Design Insights",
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
 export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
+  TITLE: "Trey Granderson - Professional Work Experience and Roles",
+  DESCRIPTION:
+    "Trey Granderson’s professional journey and work experience, detailing his roles and contributions across various organizations as a Front End Engineer. This page showcases a timeline of my work experience, highlighting key projects, technologies used, and the impact of my solutions on business objectives and user experiences.",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
+  TITLE: "Projects by Trey Granderson: Case Studies of Front End Engineering",
   DESCRIPTION:
-    "A collection of my projects, with links to repositories and demos.",
+    "Case studies and projects by Trey Granderson. Each project illustrates my approach to solving technical challenges with modern technologies and strategic thinking.",
 };
 
 export const SOCIALS: Socials = [
